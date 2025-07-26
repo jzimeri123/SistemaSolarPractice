cd7c1853c09d8884659cd1a9b0422d20f9ba7066
jzimeri123 <jzimeri@gmail.com>
2025-07-26 07:04:48 -0400

    Solar System basic structure is ready

---
+++ src/test/java/BasicStructure.java
@@ -0,0 +1,6 @@
+public class BasicStructure {
+    String moon;
+    String sun;
+    String planets;
+    String stars;
+}
---
+++ src/test/java/SolarSystem.java
@@ -0,0 +1,10 @@
+public class SolarSystem {
+    public static void main(String[] args) {
+        BasicStructure BB=new BasicStructure();
+        BB.planets="2";
+        BB.moon="2";
+        BB.stars="10";
+        BB.sun="1";
+        System.out.println("Solar system is ready");
+    }
+}
